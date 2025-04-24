2e1a412efc358b5f3e71bdd7bf917d705dd6af07
zyZ <46803451+zzy1099207684@users.noreply.github.com>
2025-04-24 16:55:52 +0300

    add using example

--- json/Json_handler.h
+++ json/Json_handler.h
@@ -2,6 +2,12 @@
 // Created by zhiyo on 2025/4/24.
 //
 
+//using example
+//Json_handler json_handler;
+//json_handler.get_value_from_key(temp_buffer, "key");
+//if (json_handler.get_result() != JSONSuccess) return;
+//char *value = json_handler.get_final_result();
+
 #ifndef JSON_HANDLER_H
 #define JSON_HANDLER_H
 #include <core_json.h>
@@ -9,10 +15,10 @@
 
 class Json_handler {
     private:
-        char *value = nullptr;
+        char *value = nullptr; 
         size_t valueLength;
         JSONStatus_t result;
-        char final_result[JSON_BUFFER_SIZE] = {0};
+        char final_result[JSON_BUFFER_SIZE] = {0};// truth value for key
     public:
         Json_handler() = default;
 
